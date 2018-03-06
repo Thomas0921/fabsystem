beaa94c000861f7e0645b95becbaae88a2b3fb4a
Thomas0921 <thomaskim092150@gmail.com>
2018-03-06 17:59:17 +0800

    update sql phpmyadmin

--- fabsystem.sql
+++ fabsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 06, 2018 at 10:18 AM
+-- Generation Time: Mar 06, 2018 at 10:58 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.2
 
@@ -25,67 +25,13 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customers`
---
-
-CREATE TABLE `customers` (
-  `customer_id` int(11) NOT NULL,
-  `customer_name` varchar(50) NOT NULL,
-  `customer_address` varchar(100) NOT NULL,
-  `customer_contact` int(50) NOT NULL,
-  `order_id` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `food_categories`
---
-
-CREATE TABLE `food_categories` (
-  `category_id` int(11) NOT NULL,
-  `category_name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `food_categories`
---
-
-INSERT INTO `food_categories` (`category_id`, `category_name`) VALUES
-(1, 'Rice'),
-(2, 'Indo'),
-(3, 'Local'),
-(4, 'Snack'),
-(5, 'Soup'),
-(6, 'Drink');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orders`
---
-
-CREATE TABLE `orders` (
-  `order_id` int(11) NOT NULL,
-  `customer_id` int(11) NOT NULL,
-  `order_time` date NOT NULL,
-  `delivery_time` date NOT NULL,
-  `order_gross` decimal(65,0) NOT NULL,
-  `order_discount` decimal(65,0) NOT NULL,
-  `order_delivery` decimal(65,0) NOT NULL,
-  `rider_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `rice_categories`
 --
 
 CREATE TABLE `rice_categories` (
   `category_id` int(11) NOT NULL,
   `category_name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `rice_categories`
@@ -97,57 +43,9 @@ INSERT INTO `rice_categories` (`category_id`, `category_name`) VALUES
 (3, 'Prawn'),
 (4, 'Sotong');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `riders`
---
-
-CREATE TABLE `riders` (
-  `rider_id` int(11) NOT NULL,
-  `rider_name` varchar(50) NOT NULL,
-  `order_id` int(11) NOT NULL,
-  `rider_earned` decimal(65,0) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
-  `user_first` varchar(50) NOT NULL,
-  `user_last` varchar(50) NOT NULL,
-  `user_email` varchar(50) NOT NULL,
-  `user_pwd` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `customers`
---
-ALTER TABLE `customers`
-  ADD PRIMARY KEY (`customer_id`),
-  ADD KEY `order_id` (`order_id`);
-
---
--- Indexes for table `food_categories`
---
-ALTER TABLE `food_categories`
-  ADD PRIMARY KEY (`category_id`);
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`order_id`),
-  ADD KEY `customer_id` (`customer_id`),
-  ADD KEY `rider_id` (`rider_id`);
 
 --
 -- Indexes for table `rice_categories`
@@ -156,80 +54,14 @@ ALTER TABLE `rice_categories`
   ADD PRIMARY KEY (`category_id`);
 
 --
--- Indexes for table `riders`
---
-ALTER TABLE `riders`
-  ADD PRIMARY KEY (`rider_id`),
-  ADD KEY `order_id` (`order_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `customers`
---
-ALTER TABLE `customers`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `food_categories`
---
-ALTER TABLE `food_categories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `orders`
---
-ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `rice_categories`
 --
 ALTER TABLE `rice_categories`
   MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `riders`
---
-ALTER TABLE `riders`
-  MODIFY `rider_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `customers`
---
-ALTER TABLE `customers`
-  ADD CONSTRAINT `customers_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
-
---
--- Constraints for table `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`),
-  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`rider_id`) REFERENCES `riders` (`rider_id`);
-
---
--- Constraints for table `riders`
---
-ALTER TABLE `riders`
-  ADD CONSTRAINT `riders_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
