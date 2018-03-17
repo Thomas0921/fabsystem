eabea8c640dbcd0fff670e30aebf9b16190497d8
Thomas0921 <thomaskim092150@gmail.com>
2018-03-18 00:00:08 +0800

    monitor calendar first got table now

--- fabsystem/public/js/monitor.js
+++ fabsystem/public/js/monitor.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
   function load_data(query, date)
 {
   $.ajax({
-  url:"../controller/fetch.php",
+  url:"../controller/AJAXmonitorfetch.php",
   method:"POST",
   data:{
      query:query,
@@ -23,7 +23,7 @@ $(document).ready(function(){
 }
 
 
-  $('#search_text').keyup(function(){
+$('#search_text').keyup(function(){
   var search = $(this).val();
   var date = $("#date_picker").val();
   console.log(search);
@@ -42,58 +42,75 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-      //Iterate through each of the rows
-       $('tr').each(function(){
-         //Check the value of the last <td> element in the row (trimmed to ignore white-space)
-         if($(this).find('td').text().trim() === "1"){
-             //Set the row to green
-             $(this).css('background','#ffe800');
-         }
-         if ($(this).find('td').text().trim() === "2") {
-           $(this).css('background','#37d726');
-         }
-         if ($(this).find('td').text().trim() === "3") {
-           $(this).css('background','#0775c8');
-         }
-         if ($(this).find('td').text().trim() === "4") {
-           $(this).css('background','#a0a0a0');
-         }
-         if ($(this).find('td').text().trim() === "5") {
-           $(this).css('background','gray');
-         }
-       });
-   });
-   //subtotal
-        var sum = 0;
-        $('#first .nettSum').each(function()
-        {
-        sum += parseFloat($(this).html());
+  //Iterate through each of the rows
+$('tr').each(function(){
+      //Check the value of the last <td> element in the row (trimmed to ignore white-space)
+      if($(this).find('td').text().trim() === "1"){
+          //Set the row to green
+          $(this).css('background','#ffe800');
+      }
+      if ($(this).find('td').text().trim() === "2") {
+        $(this).css('background','#37d726');
+      }
+      if ($(this).find('td').text().trim() === "3") {
+        $(this).css('background','#0775c8');
+      }
+      if ($(this).find('td').text().trim() === "4") {
+        $(this).css('background','#a0a0a0');
+      }
+      if ($(this).find('td').text().trim() === "5") {
+        $(this).css('background','gray');
+      }
+    });
+});
+//subtotal
+var sum = 0;
+$('#first .nettSum').each(function()
+{
+sum += parseFloat($(this).html());
 
-        });
-    //total delivery
-      var sum = 0;
-      $('#first .netDelivery').each(function()
-      {
-        sum += parseFloat($(this).html());
+});
+//total delivery
+var sum = 0;
+$('#first .netDelivery').each(function(){
+    sum += parseFloat($(this).html());
 
-      });
-      $('#totalDelivery').html(sum);
+});
 
-      $('#date_picker').on("change", function(){
+$('#totalDelivery').html(sum);
 
-      var query = $("#search_text").val();
-      var date = $(this).val();
+$('#date_picker').on("change", function(){
+
+  var query = $("#search_text").val();
+  var date = $(this).val();
 
     $.ajax({
-     url:"../controller/fetch.php",
-     method:"POST",
-     data:{
-       query:query,
-       date:date
-     },
-     success:function(data)
-     {
+      url:"../controller/AJAXmonitorfetch.php",
+      method:"POST",
+      data:{
+        query:query,
+        date:date
+      },
+      success:function(data)
+      {
       $('#result').html(data);
-     }
+      }
     });
+});
+
+$("#date_picker").each(function(){
+  var query = "";
+  var date = $(this).val();
+  $.ajax({
+    url:"../controller/AJAXmonitorfetch.php",
+    method:"POST",
+    data:{
+      query:query,
+      date:date
+    },
+    success:function(data)
+    {
+    $('#result').html(data);
+    }
   });
+});
