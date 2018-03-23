35f89c1a4a791a4b7a68771887cef7e08f2ad64a
Kennyong961127 <kenny961127@hotmail.co.uk>
2018-03-24 01:21:20 +0800

    all clear ...u test again

--- fabsystem/helper/loginsystem/login.php
+++ fabsystem/helper/loginsystem/login.php
@@ -1,4 +1,5 @@
 <?php
+
 /* User login process, checks if user exists and password is correct */
 
 // Escape email to protect against SQL injections
@@ -6,8 +7,10 @@ $email = mysqli_real_escape_string($conn, $_POST['email']);
 $result = mysqli_query($conn, "SELECT * FROM users WHERE user_email='$email'");
 
 if ( $result->num_rows == 0 ){ // User doesn't exist
+    
     $_SESSION['message'] = "User with that email doesn't exist!";
-    header("location: error.php");
+    echo "<script>location='error.php'</script>";
+    
 }
 else { // User exists
     $user = $result->fetch_assoc();
@@ -21,11 +24,14 @@ else { // User exists
 
         // This is how we'll know the user is logged in
         $_SESSION['logged_in'] = true;
-
-        header("location: ../../application/view/monitor.php");
+        
+        echo "<script>location='../../application/view/monitor.php'</script>";
     }
     else {
+        
         $_SESSION['message'] = "You have entered wrong password, try again!";
-        header("location: error.php");
+        echo "<script>location='error.php'</script>";
+        
     }
 }
+?>
--- fabsystem/helper/loginsystem/register.php
+++ fabsystem/helper/loginsystem/register.php
@@ -1,4 +1,10 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../PHPMailer-master/src/Exception.php';
+require '../PHPMailer-master/src/PHPMailer.php';
+require '../PHPMailer-master/src/SMTP.php';
 /* Registration process, inserts user info into the database
    and sends account confirmation email message
  */
@@ -21,9 +27,8 @@ $result = mysqli_query($conn, "SELECT * FROM users WHERE user_email='$email'") o
 
 // We know user email exists if the rows returned are more than 0
 if ( $result->num_rows > 0 ) {
-
     $_SESSION['message'] = 'User with this email already exists!';
-    header("location: error.php");
+    echo "<script>location='error.php'</script>";
 
 }
 else { // Email doesn't already exist in a database, proceed...
@@ -33,7 +38,7 @@ else { // Email doesn't already exist in a database, proceed...
             . "VALUES ('$first_name','$last_name','$email','$password', '$hash')";
 
     // Add user to the database
-    if (mysqli_query($conn, $sql) ){
+    if (mysqli_query($conn, $sql)) {
 
         $_SESSION['active'] = 0; //0 until user activates their account with verify.php
         $_SESSION['logged_in'] = true; // So we know the user has logged in
@@ -43,26 +48,37 @@ else { // Email doesn't already exist in a database, proceed...
                  your account by clicking on the link in the message!";
 
         // Send registration confirmation link (verify.php)
-        $to      = $email;
-        $subject = 'Account Verification ';
-        $message_body = '
-        Hello '.$first_name.',
 
-        Thank you for signing up!
 
-        Please click this link to activate your account:
-
-        http://localhost/fabsystem/fabsystem/helper/loginsystem/verify.php?email='.$email.'&hash='.$hash;
-
-        mail( $to, $subject, $message_body );
-
-        header("location: profile.php");
-
+        $verify = $hash;
+        $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+        try {
+            //Server settings
+            $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+            $mail->isSMTP();                                      // Set mailer to use SMTP
+            $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+            $mail->SMTPAuth = true;                               // Enable SMTP authentication
+            $mail->Username = 'example@gmail.com';                 // SMTP username
+            $mail->Password = 'password';                           // SMTP password
+            $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+            $mail->Port = 587;                                    // TCP port to connect to
+            //Recipients
+            $mail->setFrom('example@gmail.com', 'Mailer');
+            $mail->addAddress($_POST['email']);     // Add a recipient
+          //Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Account Verification';
+            $mail->Body    = 'Thank you for signing up!
+                            Please click this link to activate your account: https://ktdemo.000webhostapp.com/fabsystem/helper/loginsystem/verify.php?email='.$email.'&hash='.$verify;
+            $mail->send();
+            echo "<script>location='profile.php'</script>";
+      }
+      catch (Exception $e){ }
     }
-
-    else {
+      else {
         $_SESSION['message'] = 'Registration failed!';
-        header("location: error.php");
+        echo "<script>location='error.php'</script>";
+
     }
 
 }
